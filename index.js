0c7597a0de6345c18330fdb2d614717a29ed3b93
ZengJia <86162890+jiazengp@users.noreply.github.com>
2022-07-22 19:19:13 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
   return handler(request, response, {
-    public: './imgs',
     renderSingle: true
   });
 });
