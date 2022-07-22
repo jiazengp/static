8594aee00df3fa7202c0d1ed14117366eba09fe3
ZengJia <86162890+jiazengp@users.noreply.github.com>
2022-07-22 19:28:42 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const handler = require('serve-handler');
-const http = require('http');
-
-const server = http.createServer((request, response) => {
-  return handler(request, response, {
-    renderSingle: true
-  });
-});
-
-server.listen(3000, () => {
-  console.log('https service listening on port 3000');
-});
+exec('npx serve')
